7a9d08cf209c7975ec334e885c2349edd8650096
Matias Galarza <lobinuxsoft@gmail.com>
2022-04-11 23:16:26 -0300

    Modified camera behaviour and implement local multiplayer

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,10 +1,16 @@
+using System;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    [SerializeField] Vector3 offset;
+    [SerializeField] private Transform target1;
+    [SerializeField] private Transform target2;
+    [SerializeField] private Vector3 minOffset;
+    [SerializeField] private Vector3 maxOffset;
+    [SerializeField] private Vector3 lookAtOffset;
+    [SerializeField] private float maxDistance = 20;
+    [SerializeField] private AnimationCurve cameraOffsetBehaviour;
 
     // Update is called once per frame
     void LateUpdate()
@@ -14,10 +20,18 @@ public class CameraFollow : MonoBehaviour
 
     private void UpdatePosition()
     {
-        if(target)
+        if(target1 && target2)
         {
-            transform.LookAt(target);
-            transform.position = target.position + offset;
+            Vector3 middlePoint = Vector3.Lerp(target1.position, target2.position, .5f);
+            
+            float distance = Vector3.Distance(target1.position, target2.position);
+
+            Vector3 resultOffset = Vector3.Lerp(minOffset, maxOffset, cameraOffsetBehaviour.Evaluate(distance / maxDistance));
+
+            Vector3 newPos =  resultOffset + middlePoint;
+
+            transform.LookAt(middlePoint + lookAtOffset);
+            transform.position = newPos;
         }
     }
 }
